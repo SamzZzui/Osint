8723924fdbda85cc67a42e58edf86e6a626ffe0f
SamzZzui <princemelgandi1112@gmail.com>
2025-08-14 14:08:54 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,6 +1,20 @@
-# /data/data/com.termux/files/home/Osint/modules/__init__.py
+# modules/__init__.py
+# Automatically import all modules so they can be used in main.py
 
-# This file can be empty, or you can import commonly used modules here
-# Example (optional):
-# from . import domain_lookup
-# from . import subdomain_enum
+from . import domain_lookup
+from . import ip_info
+from . import email_search
+from . import social_media_search
+from . import search_phone_number
+from . import data_breach_search
+from . import people_search
+from . import image_lookup
+from . import image_metadata_lookup
+from . import email_internet_search
+
+# Optional: define what gets imported with 'from modules import *'
+__all__ = [
+    "domain_lookup", "ip_info", "email_search", "social_media_search",
+    "search_phone_number", "data_breach_search", "people_search",
+    "image_lookup", "image_metadata_lookup", "email_internet_search"
+]
