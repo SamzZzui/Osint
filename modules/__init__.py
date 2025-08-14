90f89a019dfb2c773ad6b584b0aac908983d1f8c
SamzZzui <princemelgandi1112@gmail.com>
2025-08-14 14:02:38 +0530

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,1 +1,6 @@
-# Empty file,
+# /data/data/com.termux/files/home/Osint/modules/__init__.py
+
+# This file can be empty, or you can import commonly used modules here
+# Example (optional):
+# from . import domain_lookup
+# from . import subdomain_enum
